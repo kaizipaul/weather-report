cbce6d04271909383dc1a6ab23d4b06036fee23c
kaizipaul <kaizipaul@gmail.com>
2023-07-29 23:38:19 +0300

    Add searchbar input field and state

--- src/Components/searchbar/searchBar.jsx
+++ src/Components/searchbar/searchBar.jsx
@@ -1,6 +1,11 @@
-const searchBar = () => {
-  // this will have an input section where the string is accepted
-  // and converted into an identifier that fetches the specific city the user searches
-};
+import React, { useState } from 'react';
 
-export default searchBar;
+const SearchBar = () => {
+  const [city, setCity] = useState('');
+  // this component takes in a name (string) and then sends a GET request to geocoding API
+  // the results are then displayed, the lat & long are then sent as a GET request to the main API
+  return (
+    <input value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter city name" type="text" />
+  );
+};
+export default SearchBar;

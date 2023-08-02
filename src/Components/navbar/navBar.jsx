842b8c126b83812d13153220b95e3a9ec7087cc4
kaizipaul <kaizipaul@gmail.com>
2023-08-02 23:39:42 +0300

    Add UI elements to navbar and add CSS file

--- src/Components/navbar/navBar.jsx
+++ src/Components/navbar/navBar.jsx
@@ -1,5 +1,48 @@
-const navBar = () => {
-// navbar items go here
-};
+/* eslint-disable import/no-extraneous-dependencies */
+import './navbar.css';
+import React from 'react';
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+} from '@chakra-ui/react';
 
-export default navBar;
+import {
+  HamburgerIcon, StarIcon, Search2Icon, SettingsIcon,
+} from '@chakra-ui/icons';
+
+import { FaGithub } from 'react-icons/fa6';
+
+const NavBar = () => (
+  <div className="navigation">
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        aria-label="Options"
+        icon={<HamburgerIcon />}
+        variant="outline"
+      />
+      <MenuList>
+        <MenuItem icon={<StarIcon />}>
+          My Cities
+        </MenuItem>
+        <MenuItem icon={<Search2Icon />}>
+          Search
+        </MenuItem>
+        <MenuItem icon={<FaGithub />}>
+          View Source Code
+        </MenuItem>
+        <MenuItem icon={<SettingsIcon />}>
+          Settings
+        </MenuItem>
+      </MenuList>
+    </Menu>
+    <div>
+      <Search2Icon />
+    </div>
+  </div>
+);
+
+export default NavBar;

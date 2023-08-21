b42c371bef840d32fcc4d86b72c8ef902f0a0aab
kaizipaul <kaizipaul@gmail.com>
2023-08-21 17:59:22 +0300

    Display weather details on app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import './App.css';
 import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
 import NavBar from './Components/navbar/navBar';
 import WeatherDetails from './Components/mainpage/detailsPage';
+// import MyCities from './Components/mycities/myCities';
 
 function App() {
   return (
     <>
       <NavBar />
       <WeatherDetails />
+      {/* <Routes>
+        <Route exact path="/mycities" element={<MyCities />} />
+      </Routes> */}
     </>
   );
 }

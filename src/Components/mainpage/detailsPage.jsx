0c1afca41bd04bc2fa4e77370c78da4b3b9c893f
kaizipaul <kaizipaul@gmail.com>
2023-10-03 17:06:33 +0300

    Connect Redux Store to details, with errors lol

--- src/Components/mainpage/detailsPage.jsx
+++ src/Components/mainpage/detailsPage.jsx
@@ -1,78 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import './detailspage.css';
+import { UseSelector, useDispatch } from 'react-redux';
 import { FiDroplet } from 'react-icons/fi';
 import { PiWaves, PiThermometerHot } from 'react-icons/pi';
 import { Card, CardBody } from '@chakra-ui/react';
+import { getWeatherDetails, initialState } from '../../Redux/weatherdetails/detailsPage';
 
 const WeatherDetails = () => {
-  const [weatherData, setWeatherData] = useState({});
-  const [location, setLocation] = useState({});
-  // // const [forecastData, setForecastData] = useState([]);
+  const details = UseSelector((state) => state.weatherDetails);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    // You should adjust this code to handle data from your specific API.
-    const fetchWeatherData = async () => {
-      try {
-        const response = await fetch('http://api.weatherapi.com/v1/current.json?key=dc161bae885b4480a84142424231808&q=London&aqi=no');
-        const data = await response.json();
-        console.log(data);
-        setWeatherData(data.current);
-        setLocation(data.location);
-        // setForecastData(data.forecast);
-      } catch (error) {
-        console.error('Error fetching weather data:', error);
-      }
-    };
-
-    fetchWeatherData();
+    dispatch(getWeatherDetails());
   }, []);
 
   return (
-    <div className="main-page">
-      <h2 className="city-heading">{location.name}</h2>
-      <p className="temperature-main">
-        {Math.round(weatherData.temp_c)}
-        °
-      </p>
-      <div className="general-conditions">
-        {/* <h3>{weatherData.condition}</h3> */}
-        <p className="temperature-range">
-          <p>H:</p>
-          {' '}
-          <p>L:</p>
-        </p>
+    {details === initialState ? (
+      <div>
+      <p>Loading...</p>
       </div>
-      <Card backgroundColor="gray.100">
-        <CardBody>
-          <div className="current-conditions">
-            <div className="wind column">
-              <PiWaves size="2.3rem" />
-              <p className="weatherdata">
-                {Math.round(weatherData.wind_kph)}
-                km/h
-              </p>
-              <p>Wind</p>
-            </div>
-            <div className="humidity column">
-              <FiDroplet size="2.3rem" />
-              <p className="weatherdata">
-                {weatherData.humidity}
-                %
-              </p>
-              <p>Humidity</p>
-            </div>
-            <div className="sunrise column">
-              <PiThermometerHot size="2.3rem" />
-              <p className="weatherdata">
-                {Math.round(weatherData.feelslike_c)}
-                °
-              </p>
-              <p>Feels Like</p>
-            </div>
-          </div>
-        </CardBody>
-      </Card>
+    ) : (
+      <div className="main-page">
+    <h2 className="city-heading">{location.name}</h2>
+    <p className="temperature-main">
+      {Math.round(weatherData.temp_c)}
+      °
+    </p>
+    <div className="general-conditions">
+      {/* <h3>{weatherData.condition}</h3> */}
+      <p className="temperature-range">
+        <p>H:</p>
+        {' '}
+        <p>L:</p>
+      </p>
     </div>
+    <Card backgroundColor="gray.100">
+      <CardBody>
+        <div className="current-conditions">
+          <div className="wind column">
+            <PiWaves size="2.3rem" />
+            <p className="weatherdata">
+              {Math.round(weatherData.wind_kph)}
+              km/h
+            </p>
+          </div>
+          <div className="humidity column">
+            <FiDroplet size="2.3rem" />
+            <p className="weatherdata">
+              {weatherData.humidity}
+              %
+            </p>
+          </div>
+          <div className="sunrise column">
+            <PiThermometerHot size="2.3rem" />
+            <p className="weatherdata">
+              {Math.round(weatherData.feelslike_c)}
+              °
+            </p>
+          </div>
+        </div>
+      </CardBody>
+    </Card>
+  </div>
+)};
   );
 };
 

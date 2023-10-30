1d488ebdff1244bab10e4c5754c232431523993d
kaizipaul <kaizipaul@gmail.com>
2023-10-30 13:54:51 +0300

    Update Redux Store

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,12 +1,13 @@
-import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
+import { configureStore } from '@reduxjs/toolkit';
+// import logger from 'redux-logger';
 import { detailsSlice } from './weatherdetails/detailsPage';
+import { cardSlice } from './weatherCard/weatherCard';
 
 const store = configureStore({
   reducer: {
     weatherDetails: detailsSlice,
+    weatherCard: cardSlice,
   },
-},
-applyMiddleware(logger));
+});
 
 export default store;

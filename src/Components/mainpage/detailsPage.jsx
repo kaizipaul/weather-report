4ea7f4016bd78ac3f31caf539baafbf00f84cb99
kaizipaul <kaizipaul@gmail.com>
2023-10-30 13:55:45 +0300

    Update weatherDetails and index.js

--- src/Components/mainpage/detailsPage.jsx
+++ src/Components/mainpage/detailsPage.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './detailspage.css';
-import { UseSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { FiDroplet } from 'react-icons/fi';
 import { PiWaves, PiThermometerHot } from 'react-icons/pi';
 import { Card, CardBody } from '@chakra-ui/react';
 import { getWeatherDetails, initialState } from '../../Redux/weatherdetails/detailsPage';
 
 const WeatherDetails = () => {
-  const details = UseSelector((state) => state.weatherDetails);
+  const details = useSelector((state) => state.weatherDetails);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,54 +15,57 @@ const WeatherDetails = () => {
   }, []);
 
   return (
-    {details === initialState ? (
-      <div>
-      <p>Loading...</p>
-      </div>
-    ) : (
-      <div className="main-page">
-    <h2 className="city-heading">{location.name}</h2>
-    <p className="temperature-main">
-      {Math.round(weatherData.temp_c)}
-      °
-    </p>
-    <div className="general-conditions">
-      {/* <h3>{weatherData.condition}</h3> */}
-      <p className="temperature-range">
-        <p>H:</p>
-        {' '}
-        <p>L:</p>
-      </p>
-    </div>
-    <Card backgroundColor="gray.100">
-      <CardBody>
-        <div className="current-conditions">
-          <div className="wind column">
-            <PiWaves size="2.3rem" />
-            <p className="weatherdata">
-              {Math.round(weatherData.wind_kph)}
-              km/h
-            </p>
-          </div>
-          <div className="humidity column">
-            <FiDroplet size="2.3rem" />
-            <p className="weatherdata">
-              {weatherData.humidity}
-              %
-            </p>
-          </div>
-          <div className="sunrise column">
-            <PiThermometerHot size="2.3rem" />
-            <p className="weatherdata">
-              {Math.round(weatherData.feelslike_c)}
-              °
-            </p>
-          </div>
+    <div>
+      {details === initialState ? (
+        <div>
+          <p>Loading...</p>
         </div>
-      </CardBody>
-    </Card>
-  </div>
-)};
+      ) : (
+        <div className="main-page">
+          <h2 className="city-heading">{details.name}</h2>
+          <p className="temperature-main">
+            {Math.round(details.temp_c)}
+            °
+          </p>
+          <div className="general-conditions">
+            {/* <h3>{weatherData.condition}</h3> */}
+            <p className="temperature-range">
+              <p>H:</p>
+              {' '}
+              <p>L:</p>
+            </p>
+          </div>
+          <Card backgroundColor="gray.100">
+            <CardBody>
+              <div className="current-conditions">
+                <div className="wind column">
+                  <PiWaves size="2.3rem" />
+                  <p className="weatherdata">
+                    {Math.round(details.wind_kph)}
+                    km/h
+                  </p>
+                </div>
+                <div className="humidity column">
+                  <FiDroplet size="2.3rem" />
+                  <p className="weatherdata">
+                    {details.humidity}
+                    %
+                  </p>
+                </div>
+                <div className="sunrise column">
+                  <PiThermometerHot size="2.3rem" />
+                  <p className="weatherdata">
+                    {Math.round(details.feelslike_c)}
+                    °
+                  </p>
+                </div>
+              </div>
+            </CardBody>
+          </Card>
+        </div>
+      )}
+      ;
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/configureStore';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +12,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,

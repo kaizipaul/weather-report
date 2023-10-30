eb6058456e27cfb92fdee44261170abcfeff77e2
kaizipaul <kaizipaul@gmail.com>
2023-10-30 13:54:22 +0300

    Add code for weatherCard

--- src/Components/weathercard/weatherCard.jsx
+++ src/Components/weathercard/weatherCard.jsx
@@ -1,6 +1,44 @@
-const weatherCard = () => {
-  // weather card when user adds the city searched
-  // this weather data is fethced through useDispatch from the redux store
+import React, { useEffect } from 'react';
+import './WeatherCard.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { getWeatherCard } from '../../Redux/weatherCard/weatherCard';
+
+const WeatherCard = () => {
+  const card = useSelector((state) => state.weatherCard);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getWeatherCard());
+  }, []);
+
+  return (
+    <div className="weather-card">
+      <h2>
+        {card.name}
+        {' '}
+        Weather
+      </h2>
+      <p>
+        Temperature:
+        {card.temperature}
+        °C
+      </p>
+      <p>
+        Conditions:
+        {card.conditions}
+      </p>
+      <p>
+        High:
+        {card.humidity}
+        °C
+      </p>
+      <p>
+        Low:
+        {card.humidity}
+        °C
+      </p>
+    </div>
+  );
 };
 
-export default weatherCard;
+export default WeatherCard;

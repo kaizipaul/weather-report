80ba50eac1c3db6c9b82eb888ba9ecebf579d102
kaizipaul <kaizipaul@gmail.com>
2023-11-05 15:39:56 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import './App.css';
 import React from 'react';
-// import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import NavBar from './Components/navbar/navBar';
 import WeatherDetails from './Components/mainpage/detailsPage';
-// import MyCities from './Components/mycities/myCities';
+import MyCities from './Components/mycities/myCities';
 
 function App() {
   return (
     <>
       <NavBar />
-      <WeatherDetails />
-      {/* <Routes>
+      <MyCities />
+      <Routes>
         <Route exact path="/mycities" element={<MyCities />} />
-      </Routes> */}
+      </Routes>
     </>
   );
 }

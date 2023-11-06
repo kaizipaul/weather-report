633104a16a5f09f26349e95ac807bebf6631e148
kaizipaul <kaizipaul@gmail.com>
2023-11-06 13:43:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NavBar from './Components/navbar/navBar';
-import WeatherDetails from './Components/mainpage/detailsPage';
+// import WeatherDetails from './Components/mainpage/detailsPage';
 import MyCities from './Components/mycities/myCities';
+import SearchBar from './Components/searchbar/searchBar';
 
 function App() {
   return (
     <>
       <NavBar />
       <MyCities />
+      <SearchBar />
       <Routes>
         <Route exact path="/mycities" element={<MyCities />} />
       </Routes>

728a4739262224a1407d3e8826f6bcfef76c9142
kaizipaul <kaizipaul@gmail.com>
2023-11-18 01:10:17 +0300

    style mycities page

--- src/Components/mycities/myCities.jsx
+++ src/Components/mycities/myCities.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './myCities.css';
 
 const MyCities = () => (
-  <>
-    <h2>My Cities</h2>
-    <p>No Cities were added</p>
-  </>
+  <div className="section">
+    <h2 className="header">MY CITIES</h2>
+    <p className="details">No Cities were added</p>
+  </div>
 );
 
 export default MyCities;

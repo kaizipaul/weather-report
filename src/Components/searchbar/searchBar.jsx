a00e20e1077668387745165c0f85b7cd1c0cecfa
Paul Kaizirege <kaizipaul@gmail.com>
2024-02-26 20:42:43 +0300

    add UI component and install dependencies

--- src/Components/searchbar/searchBar.jsx
+++ src/Components/searchbar/searchBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Sheet from 'react-modal-sheet';
 import { Link } from 'react-router-dom';
 import {
   Input, List, ListItem, ListIcon,
@@ -7,11 +8,12 @@ import {
 import { FaLocationArrow } from 'react-icons/fa6';
 import { getCityName, initialState } from '../../Redux/search/searchBar';
 import './searchBar.css';
-// import WeatherDetails from '../mainpage/detailsPage';
+import WeatherDetails from '../mainpage/detailsPage';
 
 function Search() {
   // local state to store the searched item
   const [searchTerm, setSearchTerm] = useState('');
+  const [isOpen, setOpen] = useState(false);
   // dispatch function to execute an action from the search reducer
   const dispatch = useDispatch();
   // accessing the reducer state
@@ -25,6 +27,17 @@ function Search() {
   return (
     <div className="search-container">
       <h3 className="header">SEARCH</h3>
+      <button type="button" onClick={() => setOpen(true)}>Open sheet</button>
+
+      <Sheet isOpen={isOpen} onClose={() => setOpen(false)}>
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>
+            <WeatherDetails />
+          </Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop />
+      </Sheet>
       <Input
         variant="filled"
         placeholder="Enter a city name"

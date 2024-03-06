8238ff18130a0e42c2c14b5a769d2a06a1c76bde
Paul Kaizirege <kaizipaul@gmail.com>
2024-03-06 20:39:54 +0300

    resolve merge conflict

--- src/Redux/search/searchBar.js
+++ src/Redux/search/searchBar.js
@@ -2,12 +2,7 @@
 // searchSlice.js
 
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-<<<<<<< HEAD
-
-const apiUrl = 'https://api.weatherapi.com/v1/search.json?key=dc161bae885b4480a84142424231808';
-=======
 import { searchapi, API_KEY } from '../../api/api';
->>>>>>> dev
 
 export const getCityName = createAsyncThunk('search/getCityName',
   async (searchTerm) => {

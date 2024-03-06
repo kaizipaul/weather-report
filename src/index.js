fe9279a87d367d995051df2ea83a02a50d6dea9c
Paul Kaizirege <kaizipaul@gmail.com>
2024-03-06 20:24:12 +0300

    Add themes and styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/configureStore';
 import App from './App';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +14,7 @@ root.render(
     <BrowserRouter>
       <ChakraProvider>
         <Provider store={store}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <App />
         </Provider>
       </ChakraProvider>
